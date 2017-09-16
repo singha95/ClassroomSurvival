38d09a4fe529c524f5f167b6dcc4861d769cd5ee
elaine95 <elaine.wei95@yahoo.ca>
2017-09-16 14:14:26 -0400

    PaperPlane

---
+++ ClassroomSurvival/Assets/Scripts/ThrowPlane.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThrowPlane : MonoBehaviour {
+	bool isThrowPlane = false;
+	public Transform playerCam;
+	public float throwForce = 10;
+	private Rigidbody rb;
+
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+	}
+
+	void FixedUpdate () {
+		if (isThrowPlane) {
+			rb.isKinematic = false;
+			transform.parent = null;
+			rb.AddForce (playerCam.forward * throwForce);
+		}
+	}
+}
